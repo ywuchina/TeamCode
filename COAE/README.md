ff32d4dd8d7289e718ab8120a55fb34154d27f4d
DHQ0918 <49122837+DHQ0918@users.noreply.github.com>
2024-08-21 13:05:35 +0800

    Add files via upload

---
+++ COAE/README.md
@@ -0,0 +1,3 @@
+# Commonality Autoencoder: Learning Common Features for Change Detection from Heterogeneous Images
+
+Paper Url: https://ieeexplore.ieee.org/abstract/document/9357940
---
+++ COAE/model/model.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+
+
+class Model(object):
+    """
+
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def start_new_session(sess):
+        saver = tf.train.Saver()  # create a saver
+        global_step = 0
+
+        sess.run(tf.global_variables_initializer())
+        print('started a new session')
+
+        return saver, global_step
+
+    @staticmethod
+    def continue_previous_session(sess, ckpt_file):
+        saver = tf.train.Saver()  # create a saver
+
+        with open(ckpt_file) as file:  # read checkpoint file
+            line = file.readline()  # read the first line, which contains the file name of the latest checkpoint
+            ckpt = line.split('"')[1]
+            global_step = int(ckpt.split('-')[1])
+
+        # restore
+        saver.restore(sess, 'saver/'+ckpt)
+        print(sess)
+        print('restored from checkpoint ' + ckpt)
+        print('恢复成功！')
+
+        return saver, global_step
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
