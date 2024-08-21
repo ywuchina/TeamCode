64ce297deaa07cab68d83d28c8fab53c21812a63
DHQ0918 <49122837+DHQ0918@users.noreply.github.com>
2024-08-21 13:08:59 +0800

    Delete CommonFunction directory

--- CommonFunction/wrapTo2PiFun.m
+++
@@ -1,6 +0,0 @@
-function [ AngleNew ] = wrapTo2PiFun(Angle)
-    AngleNew = Angle; 
-    idx = find(Angle < 0 );
-    AngleNew(idx) = AngleNew(idx) + 2*pi; 
-end
-
--- CommonFunction/wrapTo360Fun.m
+++
@@ -1,6 +0,0 @@
-function [ AngleNew ] = wrapTo360Fun( Angle )
-    AngleNew = Angle; 
-    idx = find(Angle < 0 );
-    AngleNew(idx) = AngleNew(idx) + 360; 
-end
-
